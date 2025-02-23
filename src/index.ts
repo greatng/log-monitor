300248978957817976fed34784c68b26a05ad566
Paphawit Ngamchaliew <paphawit.ng@gmail.com>
2025-02-23 19:56:30 +0700

    feat: add log precessing, add errorLog and warnLog

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import * as fs from 'fs';
 
+const APP_NAME = 'LOG Monitor';
+
+function warnLog(message: string) {
+    console.warn(`[${APP_NAME}][WARN] ${message}`);
+}
+
+function errorLog(message: string, error?: Error) {
+    console.error(`[${APP_NAME}][ERROR] ${message}`, error);
+}
+
 function splitCSVcolumns(lines: string[]): string[][] {
     const columns = lines.map((line) => {
         return line.split(',');
@@ -14,18 +24,10 @@ function splitCSVLine(data: string): string[] {
     return lines;
 }
 
-function main(argv: string[]) {
-    if (argv.length === 0) {
-        console.error(
-            'Please provide a file name: ex: npm run start "log.log"'
-        );
-    }
-
-    const fileName = argv[0];
-
-    fs.readFile(fileName, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
+function prepareLogs(fullPath: string): void {
+    fs.readFile(fullPath, 'utf8', (error, data) => {
+        if (error) {
+            errorLog('Open file error', error);
             return;
         }
 
@@ -33,6 +35,23 @@ function main(argv: string[]) {
         const logs = splitCSVcolumns(lines);
 
         console.log(logs);
+    });
+}
+
+function main(argv: string[]) {
+    if (argv.length === 0) {
+        errorLog('Please provide a file name: ex: npm run start "log.log"');
+    }
+
+    const fileName = argv[0];
+
+    fs.realpath(fileName, (error, path) => {
+        if (error) {
+            errorLog('File not found', error);
+            return;
+        }
+
+        prepareLogs(path);
     });
 }
 
